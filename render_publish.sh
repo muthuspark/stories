f457ed75d34a3e2d04fc083bcb4dff174750b5c9
muthuspark <muthukrishnan.t@hotmail.com>
2025-04-20 19:04:53 +0530

    Release 2025-04-20

---
+++ render_publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e  # Exit on any error
+
+cd /home/leopard/development/blogs/stories || exit 1
+hugo
+echo "Running pagefind..."
+echo "Building site with Hugo..."
+echo "Copying ads.txt..."
+cp ads.txt public/ads.txt
+git add .
+current_date=$(date +"%Y-%m-%d")
+commit_message=${1:-"Release $current_date"}
+git commit -m "$commit_message"
+git push
+echo "Deployment completed successfully!"
